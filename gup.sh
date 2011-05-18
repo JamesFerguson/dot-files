10722245bbd47626f12f07417e47cdf3b9221f8c
JamesFerguson <james.ferguson7@gmail.com>
2011-05-18 20:09:14 +1000

    Turns out unalias gup was the only thing stopping gup.sh working. Deleted gup.zsh as it doesn't stash. Added verbosity so I know what it's doing.

--- gup.sh
+++ gup.sh
@@ -1,47 +1,63 @@
-# function gup
-# {
-#   # subshell for `set -e` and `trap`
-#   (
-#     set -e # fail immediately if there's a problem
-#     # fetch upstream changes
-#     git fetch
-#     BRANCH=$(git describe --contains --all HEAD)
-#     if [ -z "$(git config branch.$BRANCH.remote)" -o -z "$(git config branch.$BRANCH.merge)" ]
-#     then
-#       echo "\"$BRANCH\" is not a tracking branch." >&2
-#       exit 1
-#     fi
-#     # create a temp file for capturing command output
-#     TEMPFILE="`mktemp -t gup.XXXXXX`"
-#     trap '{ rm -f "$TEMPFILE"; }' EXIT
-#     # if we're behind upstream, we need to update
-#     if git status | grep "# Your branch" > "$TEMPFILE"
-#     then
-#   
-#       # extract tracking branch from message
-#       UPSTREAM=$(cat "$TEMPFILE" | cut -d "'" -f 2)
-#       if [ -z "$UPSTREAM" ]
-#       then
-#         echo Could not detect upstream branch >&2
-#         exit 1
-#       fi
-#   
-#       # stash any uncommitted changes
-#       git stash | tee "$TEMPFILE"
-#       [ "${PIPESTATUS[0]}" -eq 0 ] || exit 1
-#   
-#       # take note if anything was stashed
-#       HAVE_STASH=0
-#       grep -q "No local changes" "$TEMPFILE" || HAVE_STASH=1
-#   
-#       # rebase our changes on top of upstream, but keep any merges
-#       git rebase -p "$UPSTREAM"
-#   
-#       # restore any stashed changed
-#       [ "$HAVE_STASH" -ne 0 ] && git stash pop -q
-#   
-#     fi
-#   )
-#   dbhack
-#   git s
-# }
+unalias gup # added by oh-my-zsh git plugin
+
+function gup
+{
+  # subshell for `set -e` and `trap`
+  (
+    set -e # fail immediately if there's a problem
+    
+    echo "Fetching upstream changes"
+    git fetch
+    BRANCH=$(git describe --contains --all HEAD)
+    if [ -z "$(git config branch.$BRANCH.remote)" -o -z "$(git config branch.$BRANCH.merge)" ]
+    then
+      echo "\"$BRANCH\" is not a tracking branch." >&2
+      exit 1
+    else
+      echo "\"$BRANCH\" is a tracking branch, using it."
+    fi
+    
+    TEMPFILE="`mktemp -t gup.XXXXXX`"
+    echo "Created a temp file, \"$TEMPFILE\", for capturing command output, will delete on exit."
+    trap '{ rm -f "$TEMPFILE"; echo "Deleted $TEMPFILE" }' EXIT
+    
+    if git status | grep "# Your branch" > "$TEMPFILE"
+    then
+      # extract tracking branch from message
+      UPSTREAM=$(cat "$TEMPFILE" | cut -d "'" -f 2)
+      echo "We're behind upstream, \"$UPSTREAM\", we need to update."
+      
+      if [ -z "$UPSTREAM" ]
+      then
+        echo Could not detect upstream branch >&2
+        exit 1
+      fi
+  
+      # stash any uncommitted changes
+      git stash | tee "$TEMPFILE"
+      [ "${PIPESTATUS[0]}" -eq 0 ] || exit 1
+  
+      # take note if anything was stashed
+      HAVE_STASH=0
+      grep -q "No local changes" "$TEMPFILE" || HAVE_STASH=1
+  
+      echo "Rebasing our changes on top of upstream, but keeping any merges."
+      echo "git rebase -p \"$UPSTREAM\""
+      git rebase -p "$UPSTREAM"
+  
+      # restore any stashed changed
+      [ "$HAVE_STASH" -ne 0 ] && git stash pop -q && echo "Popped stash."
+    
+    else
+      echo "Nothing to update"
+    fi
+    
+    # if [ -f config/database.yml.local ]
+    # then
+    #   echo "Restoring config/database.yml.local"
+    #   dbhack
+    # fi 
+    
+    git s
+  )
+}
