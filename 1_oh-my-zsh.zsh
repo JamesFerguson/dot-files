c4b8c5cd68f97799098ea3fbecbbc4bceab80307
JamesFerguson <james.ferguson7@gmail.com>
2011-05-18 16:26:40 +1000

    Ensure oh-my-zsh is loaded first so its available to other sourced files.

--- 1_oh-my-zsh.zsh
+++ 1_oh-my-zsh.zsh
@@ -1,7 +1,6 @@
+# Named 1_... to be loaded first.
 ######################################################################
 #         Oh My Zsh
-#
-# 
 ######################################################################
 
 # Path to your oh-my-zsh configuration.
@@ -12,18 +11,6 @@ export ZSH=$HOME/.oh-my-zsh
 # Optionally, if you set this to "random", it'll load a random theme each
 # time that oh-my-zsh is loaded.
 export ZSH_THEME="robbyrussell"
-
-# Set to this to use case-sensitive completion
-# export CASE_SENSITIVE="true"
-
-# Comment this out to disable weekly auto-update checks
-# export DISABLE_AUTO_UPDATE="true"
-
-# Uncomment following line if you want to disable colors in ls
-# export DISABLE_LS_COLORS="true"
-
-# Uncomment following line if you want to disable autosetting terminal title.
-# export DISABLE_AUTO_TITLE="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Example format: plugins=(rails git textmate ruby lighthouse)
