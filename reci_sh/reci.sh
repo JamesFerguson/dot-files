f2cea05c3c1270abdd37b283fbab863055675cba
JamesFerguson <james.ferguson7@gmail.com>
2012-04-10 14:33:17 +1000

    Improve reci script

--- reci_sh/reci.sh
+++ reci_sh/reci.sh
@@ -1,8 +1,13 @@
 #!/bin/zsh
 
+echo "ssh -f integrity@redev.pascalpress.com.au -p 3004 -L 3456:localhost:80 -N"
 ssh -f integrity@redev.pascalpress.com.au -p 3004 -L 3456:localhost:80 -N
 
-open "http://localhost:3456"
+echo "Opening in browser..."
+open "http://reci-ext:3456"
 
 echo "Showing tunnel(s):"
 ps -ax | grep "ssh -f integrity@redev.pascalpress.com.au -p 3004 -L 3456:localhost:80 -N"
+
+echo "Jenkins is now at http://reci-ext:3456"
+
