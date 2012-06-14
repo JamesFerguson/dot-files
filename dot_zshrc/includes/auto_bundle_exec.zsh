059ae29dfd4c2a585b7c3e5ba3123f9a11159415
JamesFerguson <james.ferguson7@gmail.com>
2012-06-14 18:06:02 +1000

    remove duplicate rackup from autobundle list

--- dot_zshrc/includes/auto_bundle_exec.zsh
+++ dot_zshrc/includes/auto_bundle_exec.zsh
@@ -1,5 +1,5 @@
-bundle_commands=( spec rspec cucumber cap watchr guard rails rackup rake jammit compass rdebug rackup )
- 
+bundle_commands=( spec rspec cucumber cap watchr guard rails rackup rake jammit compass rdebug )
+
 function run_bundler_cmd () {
     if [ -e ./Gemfile ]; then
         echo "bundle exec $@"
