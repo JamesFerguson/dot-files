06a1cc3c18e9c67b0ba3d75676181fad223fdaa1
JamesFerguson <james.ferguson7@gmail.com>
2012-06-14 18:07:19 +1000

    Add dot_gitconfig w stub credentials include
     - full credentials should not be published

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ ln -fs ~/dot-files/dot_janus_rake/janus.rake ~/.janus.rake
 ln -fs ~/dot-files/reci_sh/reci.sh ~/reci.sh
 ln -fs ~/dot-files/production_slave_sh/production_slave.sh ~/production_slave.sh
 ln -fs ~/dot-files/dot_ackrc/ackrc ~/.ackrc
+ln -fs ~/dot-files/dot_gitconfig/gitconfig ~/.gitconfig
