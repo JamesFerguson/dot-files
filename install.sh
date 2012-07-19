05ec95a4375cd17ee08a3c8af351602b89ceae52
JamesFerguson <james.ferguson7@gmail.com>
2012-07-19 14:56:53 +1000

    Setup powrc. grrr

--- install.sh
+++ install.sh
@@ -11,3 +11,17 @@ ln -fs ~/dot-files/reci_sh/reci.sh ~/reci.sh
 ln -fs ~/dot-files/production_slave_sh/production_slave.sh ~/production_slave.sh
 ln -fs ~/dot-files/dot_ackrc/ackrc ~/.ackrc
 ln -fs ~/dot-files/dot_gitconfig/gitconfig ~/.gitconfig
+
+
+# powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/readingeggs/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/readingeggs2/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/rex-student/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/blake-admin/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/eggspress-data/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/media-hub/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/blake/Plastic-Chefs/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/Coding/tv-shows/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/Coding/todos/.powrc
+ln -fs ~/dot-files/dot_powrc/powrc ~/Coding/ranker/.powrc
+
