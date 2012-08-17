15998ddd2d48f102d85be4c112aaf1a71a9482f9
JamesFerguson <james.ferguson7@gmail.com>
2012-08-17 15:19:26 +1000

    Start writing up tips to show at login

---
+++ dot_zshrc/includes/z_tips.zsh
@@ -0,0 +1,28 @@
+# en/decode64
+# extract [-r]
+# dircycle
+# j /<partial dir>/
+# tab completion for gem list|update|uninstall
+# Github functions: empty_gh (new GHified repo), new_gh (GHifiy non-git dir), exist_gh (GHifiy existing repo)
+# node-docs open url for current node version docs in browser
+# tab - cli func to open a tab
+# pfd - print finder dir
+# pfs - print finder selection
+# cdf - cd to finder dir
+# pushdf - call pushd(?) on finder dir
+# quick-look - cli func for quick-look
+# man-preview - read man in Preview.app
+# rc - rails  console
+# rd -        destroy
+# rdb -       dbconsole
+# rg -        generate
+# rgm -       "        migration
+# rp -        plugin
+# ru -        runner
+# rs -        server
+# rsd -       server --debugger
+# devlog -    tail -f log/development.log
+# rdm -  rake db:migrate
+# rdbm - "    "          db:test:clone
+# rdr -  "    db:rollback
+
