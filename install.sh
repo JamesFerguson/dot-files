b97ca586550157d2e407d7b2215896eab9a952eb
JamesFerguson <james.ferguson7@gmail.com>
2012-08-17 15:15:04 +1000

    New paths to install from

--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 
-ln -fs ~/dot-files/dot_zshrc/zshrc ~/.zshrc
-ln -fs ~/dot-files/dot_powconfig/powconfig ~/.powconfig
-ln -fs ~/dot-files/dot_inputrc/inputrc ~/.inputrc
-ln -fs ~/dot-files/dot_irbrc/irbrc ~/.irbrc
-ln -fs ~/dot-files/dot_gvimrc_local/gvimrc.local ~/.gvimrc.local
-ln -fs ~/dot-files/dot_vimrc_local/vimrc.local ~/.vimrc.local
-ln -fs ~/dot-files/dot_janus_rake/janus.rake ~/.janus.rake
-ln -fs ~/dot-files/reci_sh/reci.sh ~/reci.sh
-ln -fs ~/dot-files/production_slave_sh/production_slave.sh ~/production_slave.sh
-ln -fs ~/dot-files/dot_ackrc/ackrc ~/.ackrc
-ln -fs ~/dot-files/dot_gitconfig/gitconfig ~/.gitconfig
-
+ln -fs ~/codez/dot-files/dot_zshrc/zshrc ~/.zshrc
+ln -fs ~/codez/dot-files/dot_powconfig/powconfig ~/.powconfig
+ln -fs ~/codez/dot-files/dot_inputrc/inputrc ~/.inputrc
+ln -fs ~/codez/dot-files/dot_irbrc/irbrc ~/.irbrc
+ln -fs ~/codez/dot-files/dot_gvimrc/gvimrc ~/.gvimrc
+ln -fs ~/codez/dot-files/dot_vimrc/vimrc ~/.vimrc
+ln -fs ~/codez/dot-files/reci_sh/reci.sh ~/reci.sh
+ln -fs ~/codez/dot-files/production_slave_sh/production_slave.sh ~/production_slave.sh
+ln -fs ~/codez/dot-files/dot_ackrc/ackrc ~/.ackrc
+ln -fs ~/codez/dot-files/dot_gitconfig/gitconfig ~/.gitconfig
 
 # powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/readingeggs/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/readingeggs2/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/rex-student/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/blake-admin/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/eggspress-data/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/media-hub/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/blake/Plastic-Chefs/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/Coding/tv-shows/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/Coding/todos/.powrc
-ln -fs ~/dot-files/dot_powrc/powrc ~/Coding/ranker/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/readingeggs/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/readingeggs2/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/rex-student/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/blake-admin/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/eggspress-data/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/media-hub/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/blake/Plastic-Chefs/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Coding/tv-shows/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Coding/todos/.powrc
+ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Coding/ranker/.powrc
 
