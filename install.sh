454ad2f0935d1c48df192dfa78d6b8444ffd799e
JamesFerguson <jim@jim.local>
2012-08-29 14:03:25 +1000

    Include etc/hosts in dot files

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ ln -fs ~/codez/dot-files/reci_sh/reci.sh ~/reci.sh
 ln -fs ~/codez/dot-files/production_slave_sh/production_slave.sh ~/production_slave.sh
 ln -fs ~/codez/dot-files/dot_ackrc/ackrc ~/.ackrc
 ln -fs ~/codez/dot-files/dot_gitconfig/gitconfig ~/.gitconfig
+sudo ln -fs ~/codez/dot-files/etc_hosts/hosts /etc/hosts
 
 # powrc
 ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/readingeggs/.powrc
