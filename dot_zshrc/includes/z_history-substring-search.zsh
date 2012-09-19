d302fa1fa6554d12ad248aaaf6c6e652d17e55a6
JamesFerguson <jim@The-Ocean.local>
2012-09-19 16:44:01 +1000

    Switch on searching history by substring

---
+++ dot_zshrc/includes/z_history-substring-search.zsh
@@ -0,0 +1,5 @@
+# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
+# HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
+# HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
+
+source ~/.oh-my-zsh/plugins/history-substring-search/history-substring-search.zsh
