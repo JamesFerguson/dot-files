5bf475efd53b626621176bc48a6955c5a3c009b7
JamesFerguson <james.ferguson7@gmail.com>
2013-11-02 00:29:51 +1100

    Cleanup and tweak zsh opts

--- dot_zshrc/includes/opts.zsh
+++ dot_zshrc/includes/opts.zsh
@@ -1,27 +1,48 @@
-# next lets set some enviromental/shell pref stuff up
-# setopt NOHUP
-#setopt NOTIFY
-#setopt NO_FLOW_CONTROL
-setopt APPEND_HISTORY
-# setopt AUTO_LIST		# these two should be turned off
-# setopt AUTO_REMOVE_SLASH
-# setopt AUTO_RESUME		# tries to resume command of same name
-unsetopt BG_NICE		# do NOT nice bg commands
-setopt CORRECT			# command CORRECTION
-setopt EXTENDED_HISTORY		# puts timestamps in the history
-# setopt HASH_CMDS		# turns on hashing
-setopt HIST_ALLOW_CLOBBER
-setopt HIST_REDUCE_BLANKS
-# setopt INC_APPEND_HISTORY SHARE_HISTORY
-setopt MENUCOMPLETE
+# see `man zshoptions`
+# use `setopt` to see current options
+#
+setopt AUTO_CD # if cmd unknown try to cd to it
+setopt AUTO_PUSHD # cd pushes old dir to stack
+setopt PUSHD_MINUS # reverse meaning of +/- w number when using dir stack
+setopt PUSHD_SILENT # don't print dir stack after push/pop
+setopt PUSHD_TO_HOME # pushd ~= pushd $HOME
+setopt CDABLE_VARS # if cd param not dir & doesn't begin w '/' expand it
 
-# Set/unset shell options
-setopt   notify globdots correct pushdtohome cdablevars # autolist
-setopt   correctall autocd recexact longlistjobs
-setopt   autoresume histignoredups pushdsilent noclobber
-setopt   autopushd pushdminus extendedglob rcquotes mailwarning
-unsetopt bgnice autoparamslash
+setopt ALWAYS_TO_END # move to end after completion
+unsetopt AUTO_LIST # always list choices on completion (default: on)
+# setopt AUTO_MENU # show completion menu after second tab (default: on)
+unsetopt AUTO_PARAM_SLASH # add slash when completing dirs (default: on)
+# setopt AUTO_REMOVE_SLASH # after complete ending in '/' space removes '/' (default: on)
+setopt MENU_COMPLETE # cycle through completion menu (overrides AUTO_MENU)
+setopt REC_EXACT # In completion, recognize exact matches even if they are ambiguous
 
-# Mine
+setopt EXTENDED_GLOB # '#~^' part of patterns in globs
+setopt GLOB_DOTS # don't require a leading `.' in a filename to match
 
-setopt interactivecomments # allow comments in interactive/terminal sessions
+unsetopt APPEND_HISTORY # (see SHARE_HISTORY) append rather than replace history file on exit (default: on)
+unsetopt BANG_HIST # params starting '!' expanded from history (default: on)
+setopt EXTENDED_HISTORY		# puts timestamps/durations in the history
+setopt HIST_ALLOW_CLOBBER # allow history references to clobber files (overrides NO_CLOBBER)
+setopt HIST_IGNORE_ALL_DUPS # when adding cmd to history remove all previous dupes
+setopt HIST_REDUCE_BLANKS # remove superfluous blanks from lines added to history
+# setopt INC_APPEND_HISTORY # incrementally append each line to history, not on exit
+setopt SHARE_HISTORY # commands are appended to and imported from history file (b/w shells)
+
+unsetopt CLOBBER # '>' and '>>' can destroy files, if unset must use '>!', '>|', '>>!' or '>>|'
+unsetopt CORRECT # correct the spelling of commands
+unsetopt CORRECT_ALL # correct spelling of all arguments (implies CORRECT)
+# unsetopt FLOW_CONTROL # allow output flow control via start/stop characters (usually assigned to ^S/^Q) (default: on)
+setopt IGNORE_EOF # don't exit on EOF (Ctrl-D), require 'exit', 'logout' or 10xEOF
+setopt INTERACTIVE_COMMENTS # allow comments in interactive/terminal sessions
+# setopt HASH_CMDS # cache cmd paths (default: on)
+# setopt HASH_DIRS # cache dirs containing cmds (default: on)
+setopt MAIL_WARNING # warn re new mail
+setopt RC_QUOTES # '' within single quoted strings becomes a literal single quote
+
+setopt AUTO_RESUME # tries to resume command of same name when simple word cmd
+unsetopt BG_NICE # do NOT nice bg commands
+# unsetopt CHECK_JOBS # warn about bg/suspended jobs before exit
+# unsetopt HUP # send the HUP signal to running jobs when the shell exits
+setopt LONG_LIST_JOBS # list jobs in long format
+setopt NOTIFY # report bg job status immediately, not when next printing a prompt (default: on)
+
