f317633b622d86722c442cef4ab5d129227484ca
JamesFerguson <james.ferguson7@gmail.com>
2013-11-02 15:25:38 +1100

    Add some tips

--- dot_zshrc/includes/z_tips.zsh
+++ dot_zshrc/includes/z_tips.zsh
@@ -1,22 +1,23 @@
+# cpv - verbose copy with backup if replacing via rsync
 # en/decode64
-# extract [-r]
+# extract [-r] - extract archive (autodetect type), -r to remove original, 'x' for short
 # dircycle
 # j /<partial dir>/
 # tab completion for gem list|update|uninstall
 # Github functions: empty_gh (new GHified repo), new_gh (GHifiy non-git dir), exist_gh (GHifiy existing repo)
-# node-docs open url for current node version docs in browser
+# node-docs - open url for current node version docs in browser
 # tab - cli func to open a tab
 # pfd - print finder dir
 # pfs - print finder selection
 # cdf - cd to finder dir
-# pushdf - call pushd(?) on finder dir
+# pushdf - call pushd (add dir to dirstack) on finder dir
 # quick-look - cli func for quick-look
 # man-preview - read man in Preview.app
 # rc - rails  console
 # rd -        destroy
 # rdb -       dbconsole
 # rg -        generate
-# rgm -       "        migration
+# rgm -       generate migration
 # rp -        plugin
 # ru -        runner
 # rs -        server
@@ -25,4 +26,9 @@
 # rdm -  rake db:migrate
 # rdbm - "    "          db:test:clone
 # rdr -  "    db:rollback
+# startpost - start postgresql server
+# stoppost - stop postgresql server
+# restartpost - restart postgresql server
+# urlencode - find a scripting language and urlencode arg 0
+# urldecode - vice versa
 
