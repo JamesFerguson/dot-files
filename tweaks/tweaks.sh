c718dfc7adb3f1cddbf0aedc9458d812d058334b
James Ferguson <james.ferguson7@gmail.com>
2015-12-04 11:58:22 +1100

    Easily flip trackpad orientation so it can sit in front of keyboard

--- tweaks/tweaks.sh
+++ tweaks/tweaks.sh
@@ -1,2 +1,9 @@
+#!/bin/sh
+
 # Make finder show hidden files, /usr, etc.
 defaults write com.apple.Finder AppleShowAllFiles YES
+
+# Auto detect trackpad orientation when you place five fingers on it in natural position
+echo 'Using sudo to turn on auto-detection of trackpad orientation'
+sudo defaults write com.apple.MultitouchSupport ForceAutoOrientation YES
+
