d3edbdb9545eef375467b412eb93506e381d74ea
James Ferguson <james.ferguson7@gmail.com>
2016-02-11 18:22:12 +1100

    whitespace

--- dot_zshrc/includes/auto_bundle_exec.zsh
+++ dot_zshrc/includes/auto_bundle_exec.zsh
@@ -1,16 +1,16 @@
 bundle_commands=( spec rspec cucumber cap watchr guard rails rackup rake jammit compass rdebug spring )
 
 function run_bundler_cmd () {
-    if [ -e ./Gemfile ]; then
-        echo "bundle exec $@"
-        bundle exec $@
-    else
-        echo "$@"
-        $@
-    fi
+  if [ -e ./Gemfile ]; then
+    echo "bundle exec $@"
+    bundle exec $@
+  else
+    echo "$@"
+    $@
+  fi
 }
 
 for cmd in $bundle_commands
 do
-    alias $cmd="run_bundler_cmd $cmd"
+  alias $cmd="run_bundler_cmd $cmd"
 done
