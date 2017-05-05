b5fc16239e86889a309d62b26bec2e9d465d8ebe
James Ferguson <james.ferguson7@gmail.com>
2017-05-05 17:10:01 +1000

    Setup chruby and bring bashrc and bash_profile under source control

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 ln -fs ~/codez/dot-files/dot_ackrc/ackrc ~/.ackrc
 ln -fs ~/codez/dot-files/dot_agignore/agignore ~/.agignore
+ln -fs ~/codez/dot-files/dot_bash_profile/bash_profile ~/.bash_profile
+ln -fs ~/codez/dot-files/dot_bashrc/bashrc ~/.bashrc
 ln -fs ~/codez/dot-files/dot_ctags/ctags ~/.ctags
 ln -fs ~/codez/dot-files/dot_editrc/editrc ~/.editrc
 ln -fs ~/codez/dot-files/dot_gemrc/gemrc ~/.gemrc
