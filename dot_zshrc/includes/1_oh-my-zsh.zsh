afdec50fd77bbf735992db4f18eb8f93ea9673c5
James Ferguson <james.ferguson7@gmail.com>
2017-05-08 08:56:53 +1000

    Dump some unused omz plugins and add some others

--- dot_zshrc/includes/1_oh-my-zsh.zsh
+++ dot_zshrc/includes/1_oh-my-zsh.zsh
@@ -31,13 +31,16 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Example format: plugins=(rails git textmate ruby lighthouse)
 plugins=(\
+  asdf\
+  aws\
+  bower\
   brew\
   bundler\
-  cap\
+  capistrano\
+  chruby\
   cp\
-  dircycle\
-  encode64\
-  extract\
+  docker\
+  fasd\
   gem\
   git\
   git-extras\
