f4d4ecf67b5d47356ed628f1b72dea4aae430ccf
James Ferguson <james.ferguson7@gmail.com>
2017-05-08 08:56:02 +1000

    Defunct

--- dot_zshrc/includes/reci.zsh
+++
@@ -1,5 +0,0 @@
-function reci() {
-  echo "sshuttle -r redev 192.168.0.6 192.168.0.13 192.168.0.19 192.168.0.21"
-  echo "Reci will be available at http://reci"
-  sshuttle -r redev 192.168.0.6 192.168.0.13 192.168.0.19 192.168.0.21
-}
