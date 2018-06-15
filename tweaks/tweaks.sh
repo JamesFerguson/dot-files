db9c670b2f1290ea5852015588e4967dc1db6079
James Ferguson <james.ferguson7@gmail.com>
2018-06-15 16:11:37 +1000

    Make ridiculous max files limits go away

--- tweaks/tweaks.sh
+++ tweaks/tweaks.sh
@@ -7,3 +7,8 @@ defaults write com.apple.Finder AppleShowAllFiles YES
 echo 'Using sudo to turn on auto-detection of trackpad orientation'
 sudo defaults write com.apple.MultitouchSupport ForceAutoOrientation YES
 
+# Override default maxfiles limits
+echo 'Using sudo to raise maxfiles limits to large values'
+sudo echo "" >> /etc/sysctl.conf
+sudo echo "kern.maxfiles=65536" >> /etc/sysctl.conf
+sudo echo "kern.maxfilesperproc=32768" >> /etc/sysctl.conf
