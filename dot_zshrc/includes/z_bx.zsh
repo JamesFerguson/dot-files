c8367731b9a1fca0f5da201cfb576f4a11406384
James Ferguson <89383+JamesFerguson@users.noreply.github.com>
2018-10-04 13:30:23 +1000

    Add bs setup

--- dot_zshrc/includes/z_bx.zsh
+++ dot_zshrc/includes/z_bx.zsh
@@ -3,3 +3,7 @@ eval "$($HOME/Blake/bx/bin/bx init -)"
 
 # Blake deploy tool, bd
 eval "$($HOME/Blake/bd/bin/bd init -)"
+
+# Blake secrets tool, bs
+unalias bs
+eval "$($HOME/Blake/bs/bin/bs init -)"
