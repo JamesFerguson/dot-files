a393c00dd4bc30ef9533f276ae66325a35f79c0c
JamesFerguson <james.ferguson@blake.com.au>
2019-09-11 14:58:11 +1000

    Add aliases for which and global word count

--- dot_zshrc/includes/aliases.zsh
+++ dot_zshrc/includes/aliases.zsh
@@ -9,6 +9,7 @@ alias jobs='jobs -l'
 alias pgrep='grep -P'
 alias dmesg="sudo dmesg"
 alias locate="locate -e" # leaves out missing files (when db is old)
+alias which="which -a" # show all matching paths, not just first
 
 # the silver searcher
 alias ag="nocorrect noglob ag --smart-case -C 2"
@@ -27,6 +28,7 @@ alias sp='spring'
 # global aliases
 alias -g G='| grep'
 alias -g L='| less'
+alias -g C='| wc -l'
 
 # disable zsh corrections for some commands
 alias git='nocorrect git-achievements'
@@ -36,5 +38,6 @@ alias heroku='nocorrect heroku'
 alias github='nocorrect github'
 alias gh='nocorrect gh'
 alias grb='nocorrect grb'
+
 # (provided by omz plugin I think) remove all branches that have been merged to upstream
 # alias gbda='git branch --no-color --merged | command grep -vE "^(\*|\s*(master|develop|dev)\s*$)" | command xargs -n 1 git branch -d'
