fe6fb12475d399d7d645d88f6d0fc7e510edcb67
James Ferguson <89383+JamesFerguson@users.noreply.github.com>
2019-09-11 11:55:16 +1000

    Remove unused stuff

--- production_slave_sh/production_slave.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-echo "Setting up slave tunnel..."
-ssh re-db-slave-tunnel -N &
-
-echo "\nRun reports with: \nRAILS_ENV=reports ./script/rails runner report.rb > report.csv\n"
-echo "Start console with:\nRAILS_ENV=reports console"
-
-echo "Currently this doesn't seem to work for console, but it does for a mysql client."
