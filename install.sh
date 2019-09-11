192e812e29ac9ab16afaf2cdef9626bda1e5777c
James Ferguson <89383+JamesFerguson@users.noreply.github.com>
2019-09-11 11:55:59 +1000

    Move ruby-version and tool-versions into dot_files

--- install.sh
+++ install.sh
@@ -17,8 +17,10 @@ ln -fs ~/codez/dot-files/dot_powconfig/powconfig ~/.powconfig
 # powrc below \/
 ln -fs ~/codez/dot-files/dot_rdebugrc/rdebugrc ~/.rdebugrc
 ln -fs ~/codez/dot-files/dot_rubocop_dot_yml/rubocop.yml ~/.rubocop.yml
+ln -fs ~/codez/dot-files/dot_ruby_version/ruby-version ~/.ruby-version
 ln -fs ~/codez/dot-files/dot_screenrc/screenrc ~/.screenrc
 ln -fs ~/codez/dot-files/dot_tmux_dot_conf/tmux.conf ~/.tmux.conf
+ln -fs ~/codez/dot-files/dot_tool_versions/tool-versions ~/.tool-versions
 ln -fs ~/codez/dot-files/dot_vimrc/vimrc ~/.vimrc
 ln -fs ~/codez/dot-files/dot_zshrc/zshrc ~/.zshrc
 ln -fs ~/codez/dot-files/production_slave_sh/production_slave.sh ~/production_slave.sh
@@ -27,8 +29,8 @@ ln -fs ~/codez/dot-files/git_repack_all/git-repack-all /usr/local/bin/git-repack
 sudo ln -fs ~/codez/dot-files/etc_hosts/hosts /etc/hosts
 
 # powrc
-ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/rex-student/.powrc
-ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/mathseeds/.powrc
-ln -fs ~/codez/dot-files/dot_powrc/powrc ~/codez/tv-shows/.powrc
-ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/blake-admin/.powrc
+#ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/rex-student/.powrc
+#ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/mathseeds/.powrc
+#ln -fs ~/codez/dot-files/dot_powrc/powrc ~/codez/tv-shows/.powrc
+#ln -fs ~/codez/dot-files/dot_powrc/powrc ~/Blake/blake-admin/.powrc
 
