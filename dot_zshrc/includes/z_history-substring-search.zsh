c5cc0fbda3b7ad61423a4cf037c2af5b8d757c7c
JamesFerguson <89383+JamesFerguson@users.noreply.github.com>
2019-09-19 16:51:49 +1000

    Provided by prezto plugin now

--- dot_zshrc/includes/z_history-substring-search.zsh
+++ dot_zshrc/includes/z_history-substring-search.zsh
@@ -2,4 +2,4 @@
 # HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
 # HISTORY_SUBSTRING_SEARCH_GLOBBING_FLAGS='i'
 
-source ~/.oh-my-zsh/plugins/history-substring-search/history-substring-search.plugin.zsh
+# source ~/.oh-my-zsh/plugins/history-substring-search/history-substring-search.plugin.zsh
