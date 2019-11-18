b5bcf578128d4a7118c17724faae3b273f8922cc
JamesFerguson <89383+JamesFerguson@users.noreply.github.com>
2019-11-18 11:54:31 +1100

    Tweak key bindings in terminal

--- dot_zshrc/includes/key_bindings.zsh
+++ dot_zshrc/includes/key_bindings.zsh
@@ -1,5 +1,5 @@
 # stty erase ^H &>/dev/null # breaks delete in vi
-bindkey "^[[3~" delete-char
+#bindkey "^[[3~" delete-char
 
 autoload -U compinit
 compinit
@@ -12,3 +12,28 @@ bindkey "^[[F"  end-of-line
 bindkey "^[[4~" end-of-line
 bindkey ' ' magic-space    # also do history expansion on space
 bindkey '^I' complete-word # complete on tab, leave expansion to _expand
+
+# https://unix.stackexchange.com/questions/258656/how-can-i-delete-to-a-slash-or-a-word-in-zsh
+# Alt+Backspace
+backward-kill-dir () {
+    local WORDCHARS=${WORDCHARS/\/}
+    zle backward-kill-word
+}
+zle -N backward-kill-dir
+bindkey '^[^?' backward-kill-dir
+
+# Alt+Left
+backward-word-dir () {
+    local WORDCHARS=${WORDCHARS/\/}
+    zle backward-word
+}
+zle -N backward-word-dir
+bindkey "^[[1;3C" forward-word-dir
+
+# Alt+Right
+forward-word-dir () {
+    local WORDCHARS=${WORDCHARS/\/}
+    zle forward-word
+}
+zle -N forward-word-dir
+bindkey "^[[1;3D" backward-word-dir
