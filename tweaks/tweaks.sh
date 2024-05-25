384dc5801056182a0b23e247034095483ffdcee3
JamesFerguson <>
2024-05-25 19:44:22 +1000

    Add some Finder tweaks

--- tweaks/tweaks.sh
+++ tweaks/tweaks.sh
@@ -3,11 +3,11 @@
 # Make finder show hidden files, /usr, etc.
 echo "Making finder show hidden files, /usr, etc."
 defaults write com.apple.finder AppleShowAllFiles -boolean true
-killall Finder
 
 # Auto detect trackpad orientation when you place five fingers on it in natural position
-echo 'Using sudo to turn on auto-detection of trackpad orientation'
-sudo defaults write com.apple.MultitouchSupport ForceAutoOrientation YES
+# XXX: doesn't work
+# echo 'Using sudo to turn on auto-detection of trackpad orientation'
+# sudo defaults write com.apple.MultitouchSupport ForceAutoOrientation YES
 
 echo 'Using sudo to raise maxfiles limits to large values'
 sudo echo "" >> /etc/sysctl.conf
