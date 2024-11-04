21dbd89c72c255be4aacd23927280273a853e247
James Ferguson <89383+JamesFerguson@users.noreply.github.com>
2024-11-04 17:29:52 +1100

    feat: also bump rubygems for all versions, bump asdf plugins
     - check asdf versions (not reliable atm)

--- dot_zshrc/includes/brew_refresh.zsh
+++ dot_zshrc/includes/brew_refresh.zsh
@@ -3,8 +3,21 @@
 if [[ $[RANDOM % 16] -eq 0 ]]; then
   echo "  Updating Homebrew"
   brew update | grep "Updated Homebrew"
+
   echo "  Checking for outdated formulae"
   brew outdated
-  echo "  Updating rubygems"
-  gem update --system
+
+  echo "\n\n  Updating rubygems\ncurrent ruby --->" $(asdf current ruby)
+  eval "ruby_versions=( $(asdf list ruby | sed 's/*/ /g') )"
+  for version in "${ruby_versions[@]}"; do
+    asdf shell ruby $version
+    echo "  Updating gems for ruby $version"
+    gem update --system
+  done
+
+  echo "\n\n  Updating ASDF plugin lists"
+  asdf plugin update --all
+
+  echo "  Checking ASDF installed versions against latest"
+  asdf latest --all
 fi
